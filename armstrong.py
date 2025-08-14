4bb0ad26779aa693eea61ad1c9bf04c1442d7dce
sanket kedar <sanketkedar9067@gmail.com>
2025-08-14 13:11:00 +0530

    Add armstrong_no solution

---
+++ armstrong.py
@@ -0,0 +1,11 @@
+n=int(input())
+sum=0
+temp=n
+while n>0:
+    rem = n%10
+    sum = sum + rem**3
+    n = n//10
+if sum==temp:
+    print("armstrong number")
+else:
+    print("not an armstrong number")
